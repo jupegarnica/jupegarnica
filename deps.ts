c4c2551125c44a0692022518e82cda7368138918
Jupegarnica <juan@geekshubs.com>
2021-03-12 18:01:22 +0100

    fix favicon

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as ReactDOM from "https://jspm.dev/react-dom@17.0.0"
+export * as ReactDOM from "https://jspm.dev/react-dom@17.0.1"
 
-import * as React from "https://jspm.dev/react@17.0.0"
+import * as React from "https://jspm.dev/react@17.0.1"
 
 const {default: any, ...rest} = React
 const react = React.default
