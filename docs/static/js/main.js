90dd1b0cff453ebe182d3fe36401fd1e4b7ed43c
gh-actions <>
2021-03-12 10:12:24 +0000

    update docs

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -22677,7 +22677,7 @@ function Hand() {
     return react.createElement("svg", {
         className: "hand",
         xmlns: "http://www.w3.org/2000/svg",
-        "enable-background": "new 0 0 24 24",
+        enableBackground: "new 0 0 24 24",
         height: "0.8em",
         viewBox: "0 0 24 24",
         width: "0.8em",
