71a9c2cfbf36d8bd85d8e9d024bff1fc495728a4
Jupegarnica <juan@geekshubs.com>
2022-02-23 10:03:59 +0100

    refactor home

--- serve.tsx
+++ serve.tsx
@@ -7,18 +7,18 @@ import {
 } from "https://deno.land/x/sift@0.4.3/mod.ts";
 import { join } from "https://deno.land/std@0.126.0/path/mod.ts";
 import { Ip } from "./ip/myIp.jsx";
+import {App as Home} from "./_home/App.jsx";
 
-const NotFound = () => (
-  <div>
-    <h1>Page not found</h1>
-  </div>
-);
+// const NotFound = () => (
+//   <div>
+//     <h1>Page not found</h1>
+//   </div>
+// );
 
 serve({
   "/": (req) => Response.redirect(new URL("/home", req.url), 302),
-
+  '/home': () => jsx(<Home/>),
   "/screen/:filename+": serveStatic("screen", { baseUrl: import.meta.url }),
-  "/home/:filename+": serveStatic("home", { baseUrl: import.meta.url }),
   // "/home/:filename+": serveStatic("home", { baseUrl: import.meta.url }),
   "/old/:filename+": serveStatic("old", { baseUrl: import.meta.url }),
   "/tixy/:filename+": serveStatic("tixy", { baseUrl: import.meta.url }),
@@ -26,19 +26,12 @@ serve({
   //   const post = `Hello, you visited ${params?.slug}!`;
   //   return new Response(post);
   // },
-  "/ip": (request) => {
-    const ip = request.headers.get("x-forwarded-for");
-    // console.log(request.headers);
-
-    return jsx(<Ip ip={ip} />);
-  },
+  "/ip": (request) => jsx(<Ip ip={request.headers.get("x-forwarded-for")} />),
   404: (req) => {
     if (req.url.endsWith("index.html")) {
       return new Response("Not found", { status: 404 });
     }
-
     const nextUrl = new URL(join(req.url, "./index.html"));
-
     return Response.redirect(nextUrl, 302);
   },
 });
