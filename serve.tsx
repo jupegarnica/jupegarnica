ca751acc6cb30e44305215a6db24c52203b9e287
Garn <juan@geekshubs.com>
2022-09-16 11:02:33 +0200

    fix: get ip from connInfo

--- serve.tsx
+++ serve.tsx
@@ -5,7 +5,8 @@ import {
   jsx,
   serve,
   serveStatic,
-} from "https://deno.land/x/sift@0.4.3/mod.ts";
+} from "https://deno.land/x/sift@0.6.0/mod.ts";
+
 import { render } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 
 import { join } from "https://deno.land/std@0.126.0/path/mod.ts";
@@ -31,7 +32,11 @@ serve({
   "/tixy/:filename+": serveStatic("tixy", { baseUrl: import.meta.url }),
   "/dns/:slug": async (_, params) =>
     jsx(<Dns records={await resolveDns(params?.slug)} />),
-  "/ip": (request) => jsx(<Ip ip={request.headers.get("x-forwarded-for")} />),
+  "/ip": (request, connInfo, params)  => {  
+    const addr = connInfo.remoteAddr as Deno.NetAddr;
+    const ip = addr.hostname;
+    return jsx(<Ip ip={ip} />)
+  },
 
   404: (req) => {
     if (req.url.endsWith("index.html")) {
