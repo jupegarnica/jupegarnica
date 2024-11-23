07e9ed9f67dfbb2982954a62ab549f775c4f05b2
Juan Garnica <juan@harbiz.io>
2024-11-23 19:38:27 +0100

    feat: enhance portfolio component layout with improved spacing, borders, and text styling

--- v2024/index.tsx
+++ v2024/index.tsx
@@ -28,22 +28,22 @@ export default function Portfolio() {
         <h1 className="text-4xl font-bold text-center my-10">Portfolio</h1>
         <div className="grid grid-cols-1 gap-4">
           {projects.map((project) => (
-            <div key={project.title} className="relative p-4 grid grid-cols-[8ch_1fr_35ch] group">
-              <p className="text-gray-500 text-sm">{project.date.getFullYear()}</p>
-              <div>
+            <div key={project.title} className="relative p-2 grid grid-cols-[8ch_1fr_35ch] group">
+              <p className="text-gray-500 text-sm border-r pr-2">{project.date.getFullYear()}</p>
+              <div className="border-r px-2">
                 <h2 className="text-xl font-bold">{project.title}</h2>
-                <p className="text-gray-600">{project.description}</p>
+                <p className="text-gray-600 text-sm">{project.description}</p>
               </div>
-              <a href={project.url} className="text-blue-500 text-xs self-center">{project.url}</a>
+              <a href={project.url} className="text-blue-500 text-xs self-center pl-2">{project.url}</a>
               <div className="absolute left-1/2 transform -translate-x-1/2 w-screen hidden group-hover:block" style={{
                 top: '100%',
                 zIndex: 100,
                 backgroundColor: 'white',
-                border: '1px solid #ccc' // Add border to iframe wrapper
+                border: '1px solid #ccc'
               }}>
                 <iframe
                   src={project.url}
-                  className="w-full h-[600px] border-0" // Increase height to 600px
+                  className="w-full h-[600px] border-0"
                 ></iframe>
               </div>
             </div>
