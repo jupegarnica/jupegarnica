05ca9a1af71bc4eb2b78d8c56ffcb596d6a21b76
Juan Garnica <juan@harbiz.io>
2024-11-23 19:46:23 +0100

    feat: add Deno version setup step in deploy workflow and update serve.tsx to correctly serve static files for v2024

--- serve.tsx
+++ serve.tsx
@@ -4,10 +4,10 @@ import { serveStatic } from 'hono/deno';
 const app = new Hono();
 
 app.use('/', serveStatic({ root: './v2022/_site' }));
-app.use('/v2024', serveStatic({ root: './v2024/_site' }));
 app.use('/v2020/*', serveStatic({ root: './v2020/dist/build' }));
 app.use('/v2018/*', serveStatic({ root: './v2018' }));
 app.use('/v2022', c => c.redirect('/', 301));
+app.use('/v2024/*', serveStatic({ root: './v2024/_site' }));
 
 app.use('*', serveStatic({ root: '.' }));
 
