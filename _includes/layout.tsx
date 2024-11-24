9e070c5bafa4070464e7e86d79113aa5c4d2ab6d
Juan Garnica <juan@harbiz.io>
2024-11-24 14:32:10 +0100

    chore: remove deprecated files and clean up project structure

--- _includes/layout.tsx
+++ _includes/layout.tsx
@@ -2,8 +2,8 @@ export default function Layout({ content }) {
   return (
     <html>
       <head>
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>garn.dev</title>
+        <link rel="stylesheet" type="text/css" href="./style.css" />
       </head>
       <body>{content}</body>
     </html>
--- v2024/_data/projects/commit.md
+++
@@ -1,8 +0,0 @@
----
-title: Commit
-published: 0
-draft: false
-description: Write commit messages powered by IA
-url: 'https://github.com/jupegarnica/commit'
----
-
