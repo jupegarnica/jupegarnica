254ce56a836756809ad0e99334d998810745867f
Juan Garnica <juan@harbiz.io>
2024-11-28 11:29:57 +0100

    feat: enhance screenshot capture functionality with error handling, cookie dismissal, and improved logging

--- _scripts/captureScreenshots.ts
+++ _scripts/captureScreenshots.ts
@@ -1,34 +1,71 @@
 // scripts/extract-screenshots.ts
-import puppeteer from 'npm:puppeteer-core';
+import puppeteer from "npm:puppeteer-core";
 import { ensureDir } from "jsr:@std/fs";
 import { join } from "jsr:@std/path";
-import projects from '../_data/repos.json' with { type: "json" };
+import projects from "../_data/repos.json" with { type: "json" };
 
-const screenshotsDir = './screenshots';
+const screenshotsDir = "./screenshots";
 await ensureDir(screenshotsDir);
 
 const browser = await puppeteer.launch({
-  channel: 'chrome',
+  channel: "chrome",
+  headless: false,
+
 });
+
 
 // Create an array of promises
 const promises = projects.map(async (project) => {
-  const page = await browser.newPage();
-  console.log(`Capturing screenshot for ${project.title}`);
-  const screenshotPath = join(screenshotsDir, `${project.title}.png`);
-  await page.goto(project.url, { waitUntil: 'networkidle2' });
-  await page.setViewport({ width: 1280, height: 1200 });
+  if (project.title !== "noop") return Promise.resolve();
 
-  // Wait 2 seconds if title starts with 'jupegarnica'
-  if (project.title.startsWith('jupegarnica')) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  let page = await browser.newPage();
+  try {
+    setTimeout(() => page.close(), 30_000);
+
+    console.log(`%cCapturing screenshot for ${project.title}`, "color: green");
+    const screenshotPath = join(screenshotsDir, `${project.title}.png`);
+    let res = await page.goto(project.url, { waitUntil: "networkidle2" });
+    console.log(
+      `%cLoaded ${project.url} response: ${res?.status()}`,
+      "color: blue",
+    );
+
+    if (!res || !res.ok) {
+      throw new Error(`Failed to load ${project.url}`);
+    }
+
+    await page.setViewport({ width: 1280, height: 1200 });
+
+    // Wait 2 seconds if title starts with 'jupegarnica'
+    if (project.title.startsWith("jupegarnica")) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+    // Dismiss cookies if the button exists
+    const dismissButton = await page.$$eval('#docker-announcement-bar div', elements => elements.filter(el => el.innerText.includes('x')));
+    if (dismissButton) {
+      console.log("%cDismissing cookies for", "color: yellow", project.title);
+    }
+
+    await page.screenshot({ path: screenshotPath });
+    console.log(`%cScreenshot saved to ${screenshotPath}`, "color: green");
+  } catch (error) {
+    console.error(
+      `%cError capturing screenshot for ${project.title}: ${
+        (error as Error).message
+      }`,
+      "color: red",
+    );
+  } finally {
+    console.log(`%cClosing page for ${project.title}`, "color: yellow");
+    await page.close();
+    console.log(`%cPage closed for ${project.title}`, "color: yellow");
   }
-
-  await page.screenshot({ path: screenshotPath });
-  await page.close();
 });
 
 // Wait for all screenshots to finish
+
 await Promise.all(promises);
+console.log("%cAll screenshots captured", "color: green");
 
 await browser.close();
+console.log("%cBrowser closed", "color: yellow");
